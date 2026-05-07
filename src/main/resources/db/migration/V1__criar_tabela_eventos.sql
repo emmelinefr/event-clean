ecc13c59081ff6a4217d0066d79e748749e1effd
Alexandra Emmeline <alexandraemmeline.dev@gmail.com>
2026-05-07 19:26:18 -0300

    feat: adiciona  migration V1__criar_tabela_eventos.sql

---
+++ src/main/resources/db/migration/V1__criar_tabela_eventos.sql
@@ -0,0 +1,14 @@
+CREATE TABLE eventos (
+
+    id BIGSERIAL PRIMARY KEY ,
+    nome VARCHAR(255) NOT NULL ,
+    descricao TEXT ,
+    identificador VARCHAR(255) NOT NULL UNIQUE ,
+    data_inicio TIMESTAMP NOT NULL ,
+    data_fim TIMESTAMP NOT NULL ,
+    local_evento VARCHAR(255) NOT NULL ,
+    organizador VARCHAR(255) NOT NULL ,
+    capacidade INTEGER NOT NULL ,
+    tipo VARCHAR(50) NOT NULL
+
+);
